cd01d77aa3f6f1c9258c738aee32a6cfea7238d7
Rei Ramadani <ramadanirei@gmail.com>
2024-08-19 12:35:08 +0200

    Creating the SearchBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import youtube from "./api/youtube";
 import Grid from "@mui/material/Grid";
+import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideos] = useState({ id: {}, snippet: {} });
+
   return (
     <Grid style={{ justifyContent: "center" }} container spacing={10}>
       <Grid item xs={11}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
-            {/* {SearchBar} */}
+            <SearchBar onSubmit={handleSubmit} />
           </Grid>
           <Grid item xs={8}>
             {/* {VideoDetail} */}
@@ -20,6 +25,21 @@ function App() {
       </Grid>
     </Grid>
   );
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
+        q: searchTerm,
+      },
+    });
+    setVideos(videos);
+    setSelectedVideos(videos[0]);
+  }
 }
 
 export default App;
